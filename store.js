6759134fe56868af90bb8f6b0fb19844d77edaf3
victor moreno <victormorenosamur2007@gmail.com>
2025-09-04 20:18:47 -0500

    feat: :sparkles: implementar funciones saveCart y loadCart para tener el carrito en localStorage

--- store.js
+++ store.js
@@ -1,4 +1,15 @@
 const API = 'https://fakestoreapi.com/products';
+
 function fetchProducts() {
   return fetch(API).then(res => res.json()); // trae los productos de la api
+}
+let cart = {};
+
+function saveCart() {
+  localStorage.setItem('cart', JSON.stringify(cart)); //guarda los datos en el navegador en string
+}
+
+function loadCart() {
+  cart = JSON.parse(localStorage.getItem('cart')) || {}; //lee los datos guardados y de string los convierte a objeto
+  updateCartUI(); // si no hay nada guardado lo usa un objeto vacio
 }
